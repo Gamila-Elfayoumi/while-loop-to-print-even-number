82f752a7dd3f882a504ee6e648a67e8001199055
Gamila-Elfayoumi <117528749+Gamila-Elfayoumi@users.noreply.github.com>
2023-01-03 18:22:16 +0200

    Add files via upload

---
+++ while loop to print even number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i = 0;
+	cout << "The even number between 0 and 100 are \n";
+	while (i <= 50) {
+		cout << i << endl;
+		i = i + 2;
+	}
+	system("pause");
+}
